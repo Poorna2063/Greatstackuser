334c395d2d8f1991ce74434d942b75ce74092aa2
Poorna2063 <poornachandrap543@icloud.com>
2025-07-10 03:29:36 +0530

    First Commit

--- config/inngest.js
+++ config/inngest.js
@@ -3,7 +3,7 @@ import connectDB from "./db";
 import User from "@/models/User";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcart" }); 
+export const inngest = new Inngest({ id: "Greatstackuser" }); 
 
 /// inngest function to save data to a database
 
